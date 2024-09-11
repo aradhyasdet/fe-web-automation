2517213d2031da3e9a100809d4e1cfdeb739b47d
aradhya jain <aradhya.jain2882@gmail.com>
2024-09-12 01:22:54 +0530

    Commiting first code.

---
+++ src/seleniuminfo.java
@@ -0,0 +1,94 @@
+import org.openqa.selenium.chrome.ChromeDriver;
+import org.openqa.selenium.firefox.FirefoxDriver;
+import org.openqa.selenium.WebDriver;
+public class seleniuminfo {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		//System.setProperty("webdriver.chrome.driver", "C:\\Users\\dell\\Downloads\\chromedriver-win64\\chromedriver-win64\\chromedriver.exe");
+		System.setProperty("webdriver.gecko.driver", "C:\\Users\\dell\\Downloads\\geckodriver-v0.35.0-win64\\geckodriver.exe");
+		//WebDriver driver = new ChromeDriver();
+		WebDriver driver = new FirefoxDriver();
+		driver.get("https://rahulshettyacademy.com");
+		System.out.println(driver.getTitle());
+		System.out.println(driver.getCurrentUrl());
+		System.out.println(driver.getPageSource());
+		driver.quit();
+		}
+
+}
+
+/*
+ * import org.openqa.selenium.WebDriver;
+ * 
+ * import org.openqa.selenium.chrome.ChromeDriver;
+ * 
+ * import org.openqa.selenium.edge.EdgeDriver;
+ * 
+ * import org.openqa.selenium.firefox.FirefoxDriver;
+ * 
+ * 
+ * 
+ * public class SelIntroduction {
+ * 
+ * 
+ * 
+ * public static void main(String[] args) {
+ * 
+ * //Invoking Browser
+ * 
+ * //Chrome - ChromeDriver exten->Methods close get
+ * 
+ * //Firefox- FirefoxDriver ->methods close get
+ * 
+ * // WebDriver close get
+ * 
+ * //WebDriver methods + class methods
+ * 
+ * // Chrome
+ * 
+ * ChromeOptions options = new ChromeOptions();
+ * 
+ * options.addArguments("--remote-allow-origins=*");
+ * 
+ * System.setProperty("webdriver.chrome.driver",
+ * "/Users/rahulshetty/Documents/chromedriver");
+ * 
+ * WebDriver driver = new ChromeDriver(options);
+ * 
+ * 
+ * 
+ * //Firefox
+ * 
+ * System.setProperty("webdriver.gecko.driver",
+ * "/Users/rahulshetty/Documents/geckodriver");
+ * 
+ * WebDriver driver1 = new FirefoxDriver();
+ * 
+ * //Microsoft Edge
+ * 
+ * System.setProperty("webdriver.edge.driver",
+ * "/Users/rahulshetty/Documents/msedgedriver");
+ * 
+ * WebDriver driver2 = new EdgeDriver();
+ * 
+ * driver.get("https://rahulshettyacademy.com");
+ * 
+ * System.out.println(driver.getTitle());
+ * 
+ * System.out.println(driver.getCurrentUrl());
+ * 
+ * driver.close();
+ * 
+ * //driver.quit();
+ * 
+ * 
+ * 
+ * }
+ * 
+ * 
+ * 
+ * }
+ * 
+ * 
+ */
